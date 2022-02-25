eedf97ee10eaa359fefb9115adb7aec4595ef286
Mikah Mathews <mikah@outofband.org>
2022-02-25 14:29:54 -0800

    Add test to get pastry price

--- PierresBakery.Tests/ModelTests/Pastry.Tests.cs
+++ PierresBakery.Tests/ModelTests/Pastry.Tests.cs
@@ -12,5 +12,13 @@ namespace Pierres.TestTools
       Pastry fivePastries = new Pastry(5);
       Assert.AreEqual(typeof(Pastry), fivePastries.GetType());
     }
+
+    [TestMethod]
+    public void GetPastryPrice_Returns2for1_Int()
+    {
+      int expectedNumber = 2;
+      int pastryPrice = Pastry.GetPastryPrice(1);
+      Assert.AreEqual(pastryPrice, expectedNumber); 
+    }
   }
 }
